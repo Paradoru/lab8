ca74efccac8e07a15a0d4157de727d032f2bed14
Paradoru <LicLexa@yandex.ru>
2022-06-23 20:56:31 +0200

    Добавьте файлы проекта.

---
+++ lab8/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace lab8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                matrosov.number nikita;
+                Console.WriteLine("Введите первое число:");
+                nikita.first = double.Parse(Console.ReadLine());
+                do
+                {
+                    Console.WriteLine("Введите второе число:");
+                    nikita.second = double.Parse(Console.ReadLine());
+                    try
+                    {
+                        if (nikita.second == 0)
+                        {
+                            throw new Exception("На 0 делить нельзя");
+                        }
+                        else
+                        {
+                            double a = nikita.first /= nikita.second;
+                            Console.WriteLine($"{a:F2}");
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+                }
+                while (nikita.second == 0);
+            }
+            catch
+            {
+                Console.WriteLine("Пожалуйста, вводите только цифры");
+            }
+        }
+    }
+}
+
---
+++ lab8/matrosov.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab8
+{
+    public class matrosov
+    {
+        public struct number
+        {
+            public double first;
+            public double second;
+            public number(double First, double Second) { first = First; second = Second; }
+        }
+    }
+}
